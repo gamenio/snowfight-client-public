d519ce4734773f3ad7569e05b53d3788128d7f09
Luthier <liuyanjun1016@live.cn>
2025-08-21 15:35:14 +0800

    Update ClientConfig.h

--- Classes/game/ClientConfig.h
+++ Classes/game/ClientConfig.h
@@ -27,7 +27,7 @@
 #define NTS_SERVER_PORT             18123
 
 #else
-#define NTS_SERVER_ADDR             "192.168.1.109"
+#define NTS_SERVER_ADDR             "127.0.0.1"
 #define NTS_SERVER_PORT             18123
 
 #endif // TARGET_SERVER_ENV
